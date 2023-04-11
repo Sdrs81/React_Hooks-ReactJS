bbfd40a080b8573128081abb043c20b046c5cfa0
Serdar Demirtaş <serdardemirtas25@gmail.com>
2023-04-11 16:06:02 +0300

    Custom Hook has been added

---
+++ CustomHook/src/useTitle.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {  useEffect } from 'react'
+
+
+export default function useTitle(num) {
+
+    useEffect(() => {
+        document.title = `Sayı ${num}`
+    }, [num])
+}
